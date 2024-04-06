ff64558ce90a98bb10b3d0c5976e7ffa039b16f5
MoyeMio <1154268595@qq.com>
2024-04-06 17:22:28 +0800

    update .

---
+++ build&run.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.com" PCID.sln /build "Release|x64" /project "Test/IntegrationTest/Integration_Test_001 (PCID)/Integration_Test_001 (PCID).vcxproj"
+"x64/Release/Integration_Test_001 (PCID).exe"
